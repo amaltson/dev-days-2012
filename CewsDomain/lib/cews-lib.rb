81028bd36bd6573f696a17b850422299a5b5240c
Arthur Kalmenson <arthur.kalm@gmail.com>
2012-05-24 23:22:20 -0400

    Add Maven project that integrates with JRuby
    * manually testing with jrubyc and manual jar command

--- CewsDomain/lib/cews-lib.rb
+++ CewsDomain/lib/cews-lib.rb
@@ -1,5 +1,6 @@
 require 'java'
 java_require 'cews-lib'
+java_package 'com.otpp'
 
 class JavaIntegrationTest
   java_signature 'void sayHello()'
---
+++ jruby-integration/src/main/java/ruby/integration/TestIntegration.java
@@ -0,0 +1,10 @@
+package ruby.integration;
+
+import com.otpp.JavaIntegrationTest;
+
+public class TestIntegration {
+	public static void main(String[] args) {
+		JavaIntegrationTest test = new JavaIntegrationTest();
+		test.sayHello();
+	}
+}
